e61b804cd3542e8787941741da02ef18f16fd6fa
parthivpatel1106 <parthivpatel1106@gmail.com>
2020-08-22 13:29:08 +0530

    final change

--- string_formatting.py
+++ string_formatting.py
@@ -1,11 +1,12 @@
 def print_formatted(n):
+    w = len(str(bin(n)).replace('0b','')) #this will print length of binary digits without 0b
     for i in range(n):
         deci=i+1
-        decimal=str(deci)
-        octa=str(oct(i+1)[2:])
-        hexa=hex(i+1)[2:]
-        bina=str(bin(i+1)[2:])
-        print("",decimal.rjust(2),"",octa.rjust(2),"",hexa.upper().rjust(2),bina.rjust(5))
+        decimal=str(deci).rjust(w,' ')
+        octa=str(oct(i+1)[2:]).rjust(w,' ')
+        hexa=hex(i+1)[2:].rjust(w,' ')
+        bina=str(bin(i+1)[2:]).rjust(w,' ')
+        print(decimal,octa,hexa.upper(),bina)
 
 n=int(input())
 result=print_formatted(n)
