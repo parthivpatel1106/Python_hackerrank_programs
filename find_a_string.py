6534607dae705aa04397a2460bc8e5ec94f20eff
parthivpatel1106 <parthivpatel1106@gmail.com>
2020-08-22 12:43:53 +0530

    comment added

--- find_a_string.py
+++ find_a_string.py
@@ -2,7 +2,7 @@ string=input()
 substring=input()
 count=0
 print(string[2:5])
-print(string[4:7])
+print(string[4:7]) #to check the substrings
 for i in range(len(string)-1):
     if(substring==string[i:len(substring)+i]):
         count=count+1
