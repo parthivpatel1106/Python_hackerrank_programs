edf326c9dfb06b8759076815bc5921ebd98ecb07
parthivpatel1106 <parthivpatel1106@gmail.com>
2020-08-24 17:34:03 +0530

    Final Output

--- Alphabet_rangoli.py
+++ Alphabet_rangoli.py
@@ -1,5 +1,12 @@
 n=int(input())
 alpha='abcdefghijklmnopqrstuvwxyz'
-print(alpha[n-1])
-for i in range(n):
-    print(alpha[n-1].center((n**2),'-'))
+for i in range(n,0,-1):
+    s='-'.join(alpha[i-1:n])
+    x=s[::-1]+s[1:]
+    y=x.center((n*4)-3,'-')
+    print(y)
+for i in range(1,n):
+    s='-'.join(alpha[i:n])
+    x=s[::-1]+s[1:]
+    y=x.center((n*4)-3,'-')
+    print(y)
