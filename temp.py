2d650a23558036b5d890968758d3fb9e33c37880
parthivpatel1106 <parthivpatel1106@gmail.com>
2020-08-24 17:34:17 +0530

    temporary

--- temp.py
+++ temp.py
@@ -1,9 +1,12 @@
-string=input()
-substring=input()
-count=0
-print(string[2:5])
-print(string[4:7])
-for i in range(len(string)-1):
-    if(substring==string[i:len(substring)+i]):
-        count=count+1
-print(count)
+n=int(input())
+alpha='abcdefghijklmnopqrstuvwxyz'
+for i in range(n,0,-1):
+    s='-'.join(alpha[i-1:n])
+    x=s[::-1]+s[1:]
+    y=x.center((n*4)-3,'-')
+    print(y)
+for i in range(1,n):
+    s='-'.join(alpha[i:n])
+    x=s[::-1]+s[1:]
+    y=x.center((n*4)-3,'-')
+    print(y)
